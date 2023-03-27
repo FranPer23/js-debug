0c9869fde0e2adb913a7057fefb36d0078b3b909
FranPer23 <francescaprt@yahoo.com>
2023-03-27 17:01:04 +0200

    bozza commenti bundle-2

--- bundle-1/prova.js
+++ bundle-1/prova.js
@@ -1,39 +1,44 @@
-// function addNumbers() {
-//     const userNumber = prompt('Inserisci un numero');
-//     const total = userNumber + 12;
+// // function addNumbers() {
+// //     const userNumber = prompt('Inserisci un numero');
+// //     const total = userNumber + 12;
 
-//     console.log(`Il risultato finale è ${total}`);
+// //     console.log(`Il risultato finale è ${total}`);
+// // }
+
+// const addresses = [
+//     'mymail@mail.com',
+//     'yourmail@mail.com',
+//     'hermail@mail.com',
+//     'hismail@mail.com',
+// ];
+
+// const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+// function checkAccess() {
+  
+//     //l'array si colloca fuori la funzione
+    
+
+//     // let grantAccess = 'false';
+    
+//     // if (addresses.includes(userEmail)) {
+//     //     grantAccess = 'true';
+//     // }
+    
+
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
 // }
 
-const addresses = [
-    'mymail@mail.com',
-    'yourmail@mail.com',
-    'hermail@mail.com',
-    'hismail@mail.com',
-];
+const array = [1, 2, 3, 4, 5]
 
-const userEmail = prompt('Inserisci il tuo indirizzo email');
+    for (let i = 0; i < 5; i++) {
+        console.log(array[i]);
 
-function checkAccess() {
-  
-    //l'array si colloca fuori la funzione
-    
-
-    // let grantAccess = 'false';
-    
-    // if (addresses.includes(userEmail)) {
-    //     grantAccess = 'true';
-    // }
-    
-
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
     }
-}
-
-
 
 
 
--- bundle-2/main.js
+++ bundle-2/main.js
@@ -13,19 +13,29 @@
 // ESERCIZIO 1
 for (let i = 0; i > 5; i++) {
     console.log(i);
+    //mi serve un array per far funzionare il ciclo, a quel punto posso stampare nella console: console.log(nomeArray[i])
 }
 
 
 // ESERCIZIO 2
 function addIfEven(num) {
+    //devo aggiungere una costante (?)
     if (num % 2 = 0) {
+    //num % 2 === 0
         return num + 5;
+    //return qui non serve, qui avrò il calcolo
     }
     return num;
+    //il risultato del calcolo esce fuori dalla funzione grazie a questo return
+    
 }
 
 
 // ESERCIZIO 3
+
+//mi serve un  array
+// non ho bisogno della funzione
+//una volta creato l'array >>> console.log(nomeArray[i])
 function loopToFive() {
     for (let i = 0, i < 5, i++) {
         console.log(i);
@@ -37,11 +47,17 @@ function loopToFive() {
 function displayEvenNumbers() {
     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
     let evenNumbers = [];
+    //entrambi gli array devono essere dichiarati fuori dalla funzione
     for (let i = 0; i < numbers.length - 1; i++;) {
+        //dopo i++ no ;
         if (numbers % 2 = 0); {
+        // dopo la parentesi tonda no ;
+        //il simbolo uguale deve essere ripetuto 3 volte
             evenNumbers.push(i);
+            //il push deve avvenire nell'array evenNumbers
         }
         return evenNumbers;
+        //questo return deve essere scritto prima dell'ultima graffa
     }
 }
 displayEvenNumbers(); // [2,4,6,8]
